8b782a8bf19cf4d13c9d902888d81371e6a58d54
mthasneem <mthasneem703@gmail.com>
2024-05-25 18:36:42 +0530

    first

---
+++ calc4/calc4.h
@@ -0,0 +1,33 @@
+typedef enum { typeCon, typeId, typeOpr } nodeEnum;
+
+/* Constants Node Type */
+typedef struct {
+    int value;  /* Value of the constant */
+} conNodeType;
+
+/* Identifiers Node Type */
+typedef struct {
+    int i;  /* Index in the symbol table */
+} idNodeType;
+
+/* Operators Node Type */
+typedef struct {
+    int oper;  /* Operator */
+    int nops;  /* Number of operands */
+    struct nodeTypeTag *op[1];  /* Operands, extended at runtime */
+} oprNodeType;
+
+/* General Node Type */
+typedef struct nodeTypeTag {
+    nodeEnum type;  /* Type of node: constant, identifier, or operator */
+
+    union {
+        conNodeType con;  /* Constant node */
+        idNodeType id;    /* Identifier node */
+        oprNodeType opr;  /* Operator node */
+    };
+} nodeType;
+
+/* Symbol Table */
+extern int sym[26];  /* Array to store values of variables 'a' to 'z' */
+
